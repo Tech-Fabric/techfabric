72751843e60193f4d3c49c6b35ec5fecbfe5920e
Yaroslav Zaklynskyi <yaroslav@techfabric.co>
2019-08-12 20:50:53 +0300

    Implement responsive layout and logic for the application drawer.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 // JS Goes here - ES6 supported
 
 import "./css/main.scss";
+
+const whenDocumentLoaded = function() {
+  const menuButtons = document.querySelectorAll("[data-menu-open]");
+
+  for (const button of menuButtons) {
+    const value = button.dataset.menuOpen;
+    const menu = document.querySelector("[data-menu-id=" + value + "]");
+
+    const callback = () => {
+      menu.classList.toggle("display");
+    };
+
+    button.addEventListener("mouseenter", callback);
+    button.addEventListener("mouseleave", callback);
+
+    menu.addEventListener("mouseenter", callback);
+    menu.addEventListener("mouseleave", callback);
+  }
+};
+
+if (
+  document.readyState === "complete" ||
+  (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+  whenDocumentLoaded();
+} else {
+  document.addEventListener("DOMContentLoaded", whenDocumentLoaded);
+}
