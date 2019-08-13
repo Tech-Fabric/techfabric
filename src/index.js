ce230cf75b0b5550f535dcc555236a986760e09f
Yaroslav Zaklynskyi <yaroslav@techfabric.co>
2019-08-13 14:03:38 +0300

    Complete UI functionality of navigation via 'drawer'. Add JS logic for toggling drawer. Add animated hamburger menu.

--- src/index.js
+++ src/index.js
@@ -4,20 +4,42 @@ import "./css/main.scss";
 
 const whenDocumentLoaded = function() {
   const menuButtons = document.querySelectorAll("[data-menu-open]");
+  const menuToggle = document.getElementById("drawer-toggle");
+  const menuBodies = document.querySelectorAll("[data-menu-id]");
+
+  const isMobile = window.innerWidth <= 756;
+
+  menuToggle.addEventListener("click", () => {
+    document.body.classList.toggle("no-scroll");
+    document.getElementById("drawer").classList.toggle("visible");
+    document.getElementById("drawer-toggle-label").classList.toggle("hamburger-icon_close");
+
+    for (const menu of menuBodies) {
+      menu.classList.remove("display");
+    }
+  });
 
   for (const button of menuButtons) {
     const value = button.dataset.menuOpen;
     const menu = document.querySelector("[data-menu-id=" + value + "]");
 
-    const callback = () => {
+    const callback = (event) => {
       menu.classList.toggle("display");
     };
 
-    button.addEventListener("mouseenter", callback);
-    button.addEventListener("mouseleave", callback);
+    const eventName = isMobile ? "click" : "mouseenter";
 
-    menu.addEventListener("mouseenter", callback);
-    menu.addEventListener("mouseleave", callback);
+    button.addEventListener(eventName, callback);
+
+    if (!isMobile) {
+      button.addEventListener("mouseleave", callback);
+    }
+
+    menu.addEventListener(eventName, callback);
+
+    if (!isMobile) {
+      menu.addEventListener("mouseleave", callback);
+    }
   }
 };
 
